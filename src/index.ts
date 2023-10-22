5ef4dc522c438ff0c9b9b8bd46dd3c60c8603d34
Rajnish379 <rajinish77@gmail.com>
2023-10-22 16:53:22 -0500

    Final commit

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,78 @@
-import { Elysia } from "elysia";
+import { Elysia,t } from "elysia";
+import { plugin } from "./plugin";
+import { signinDTO } from "./models";
 
+ 
+
+// APPLICATION
 const app = new Elysia().get("/", () => "Hello Bunny")
+.use(plugin)
+.state({
+  id: 1,
+  email: 'rajinish@gmail.com'
+})
+.decorate('getDate',()=> Date.now())
 .get('/post/:id',({params: {id}}) => {return {id: id, title: 'Learn Bun'}})
-.listen(3000);
+.post('/post',({body,set,store}) => {
+  console.log(store)
+  set.status = 201
+  return body
+})
+.get('/track/*',() => {return 'Track Route'})
+.get('/tracks',({store,getDate}) => {
+  
+
+   // return new Response(JSON.stringify({
+  //   "tracks" : [
+  //     'Dancing Feat',
+  //     'Sam I',
+  //     'Balle Balle',
+  //   ]
+  // }),{
+  //   headers: {
+  //     'Content-Type': 'application/json'
+  //   }
+  // })
+  // Elicia does the stringifying of json for us so we don't have to do it ourselves
+  console.log(store)
+  console.log(getDate())
+  console.log(store['plugin-version'])
+  return  {
+    "tracks": [
+    'Dancing Feat',
+    'Sam I',
+    'New'
+  ]}
+});
+
+app.group('/user',app => app
+.post('/sign-in',({body}) => body, {
+  body: signinDTO,
+  response: signinDTO
+})
+.post('/sign-up',() => "Signup Route")
+.post('/profile',() => "Profile Route")
+.get('/:id',() => 'User by id Route')
+)
+
+app.group('/v1',app => app
+.get('/', () =>  "Version 1")
+.group('/products',app => app
+.post('/',() => "Create Product")
+.get('/:id',({params: {id}}) => {
+  return id
+},
+{
+  params: t.Object({
+    id: t.Numeric()
+  })
+})
+.put('/:id',() => "UPDATE product by id")
+.delete('/:id',() => "DELETE PRODUCT BY ID")
+)
+)
+
+app.listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
---
+++ src/models.ts
@@ -0,0 +1,5 @@
+import {t} from 'elysia'
+export const signinDTO = t.Object({
+    username: t.String(),
+    password: t.String()
+  })
